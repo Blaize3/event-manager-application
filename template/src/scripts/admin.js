1131635f1dfc2f71f5042fecb925e0ad1bc09b17
Ode Akugbe <akugbeode@yahoo.com>
2017-11-16 20:53:47 +0100

    Completed implementation of modify center page

--- template/src/scripts/admin.js
+++ template/src/scripts/admin.js
@@ -46,7 +46,28 @@ function handleClickEvent(e){
       <div class="col"><div class="form-group"><input class="form-control" type="text" placeholder="Creator"></div></div><div class="col"> <div class="form-group">
                   <button type="submit" id="addEvent" onClick="addEventHandler()" class="btn btn-success">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Add Center&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button></div></div></div></div></form>`;
   }else if (menu === "Modify Centers Details"){
-    
+    headText.innerHTML = "Modify Centers Details";
+    contentArea.innerHTML = "";
+    contentArea.innerHTML = `<form class="object-margin"><div class="container objectAlign">
+    <!--  -->
+    <div class="row justify-content-center"><div class="col"><div class="form-group"><select class="custom-select d-block my-3" required>
+    <option value="">Select center to be modified</option><option value="001">001</option><option value="002">002</option></select></div></div></div>
+    <!--  -->
+    <div class="row content-table-body">&nbsp;&nbsp;&nbsp;Event Center Details</div><div class="row">
+    <div class="col"><div class="form-group">
+          <input class="form-control" type="text" placeholder="Center ID">
+      </div></div><div class="col"><div class="form-group">
+          <input class="form-control" type="text" placeholder="Center Name">
+      </div></div></div><div class="row"><div class="col">
+      <div class="form-group"><input class="form-control" type="text" placeholder="Center Location">
+      </div></div><div class="col">
+      <div class="form-group"> <input class="form-control" type="text" placeholder="Center Type">
+      </div></div></div><div class="row">
+      <div class="col"><div class="form-group"><input class="form-control" type="text" placeholder="Capacity">
+          </div></div><div class="col"><div class="form-group"><input class="form-control" type="date" placeholder="Create Date">
+          </div></div></div><div class="row">
+      <div class="col"><div class="form-group"><input class="form-control" type="text" placeholder="Creator"></div></div><div class="col"> <div class="form-group">
+                  <button type="submit" class="btn btn-success">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Modify Center&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button></div></div></div></div></form>`;
   }else if (menu === "My Profile"){
     contentArea.innerHTML = "";
   }else if (menu === "Sign Out"){
