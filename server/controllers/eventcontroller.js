376dd44243fdfd601d72f1d16de73417de262a5d
Ode Akugbe <akugbeode@yahoo.com>
2017-11-22 23:38:25 +0100

    Implemented edit event

--- server/controllers/eventcontroller.js
+++ server/controllers/eventcontroller.js
@@ -50,68 +50,68 @@ class HandleEventRequest {
 
     // ends else if
   }
-//   /**
-//  *
-//  *
-//  * @static
-//  * @param {any} request
-//  * @param {any} response
-//  * @param {any} next
-//  * @returns {HandleEventRequest} The identifier for ...
-//  * @memberof HandleEventRequest
-//  */
-//   static editEvent(request, response, next) {
-//     const editObject = {
-//       id: request.params.eventId,
-//       userId: parseInt(request.body.userId),
-//       centerId: parseInt(request.body.centerId),
-//       title: request.body.title,
-//       organizer: request.body.organizer,
-//       start_time: request.body.start_time,
-//       event_time: request.body.event_time,
-//       end_time: request.body.end_time,
-//       privacy: request.body.privacy,
-//       description: request.body.description
-//     };
-//     const validateEventCreateObject = EventValidator.createEventValidators(editObject);
-//     if (validateEventCreateObject.isNotValid) {
-//       console.log(validateEventCreateObject.isNotValid);
-//       return response.status(400).send({
-//         message: `${validateEventCreateObject.errorCount} input fields data are not properly set`,
-//         data: validateEventCreateObject.errorMessage
-//       });
-//     }
+  /**
+ *
+ *
+ * @static
+ * @param {any} request
+ * @param {any} response
+ * @param {any} next
+ * @returns {HandleEventRequest} The identifier for ...
+ * @memberof HandleEventRequest
+ */
+  static editEvent(request, response, next) {
+    const editObject = {
+      id: request.params.eventId,
+      userId: parseInt(request.body.userId),
+      centerId: parseInt(request.body.centerId),
+      title: request.body.title,
+      organizer: request.body.organizer,
+      start_time: request.body.start_time,
+      event_time: request.body.event_time,
+      end_time: request.body.end_time,
+      privacy: request.body.privacy,
+      description: request.body.description
+    };
+    const validateEventCreateObject = EventValidator.createEventValidators(editObject);
+    if (validateEventCreateObject.isNotValid) {
+      console.log(validateEventCreateObject.isNotValid);
+      return response.status(400).send({
+        message: `${validateEventCreateObject.errorCount} input fields data are not properly set`,
+        data: validateEventCreateObject.errorMessage
+      });
+    }
 
-//     //  check if authenticated user is the creator of the event
-//     if (request.decoded.userID === request.eventObject.userId) {
-//       const eventObj={
-//         ...request.eventObject.dataValues,
-//         ...editObject
-//       };
-//       return Event
-//       .update(eventObj, {
-//         where: {
-//           id: request.params.eventId
-//         },
-//         returning: true,
-//         plain: true
-//       })
-//       .then(editedObject => {
-//           return response.status(200).send({
-//           Status: 'Edit Event Successful',
-//           'Edited Event Details': editedObject[1].dataValues
-//         });
-//       })
-//       .catch((error) => {
-//         next(error);
-//       });  
-//     }else{
-//       return response.status(401).send({
-//         Status: 'Edit Event Failed',
-//         message: `User ${request.decoded.userID} is not authorized to edit this event`
-//       });
-//     }
-//   }
+    //  check if authenticated user is the creator of the event
+    if (request.decoded.userID === request.eventObject.userId) {
+      const eventObj={
+        ...request.eventObject.dataValues,
+        ...editObject
+      };
+      return Event
+      .update(eventObj, {
+        where: {
+          id: request.params.eventId
+        },
+        returning: true,
+        plain: true
+      })
+      .then(editedObject => {
+          return response.status(200).send({
+          Status: 'Edit Event Successful',
+          'Edited Event Details': editedObject[1].dataValues
+        });
+      })
+      .catch((error) => {
+        next(error);
+      });  
+    }else{
+      return response.status(401).send({
+        Status: 'Edit Event Failed',
+        message: `User ${request.decoded.userID} is not authorized to edit this event`
+      });
+    }
+  }
 //   /**
 //  *
 //  *
--- server/test/test.js
+++ server/test/test.js
@@ -1,119 +1,92 @@
-import request from 'supertest';
+import api from 'supertest';
 import { expect } from 'chai';
 import app from '../app';
 import HandleUserRequests from '../controllers/usercontroller';
 
-describe('Testing API endpoint', () => {
-  describe('Test Sign Up Route', () => {
-    describe('Handling valid case', () => {
-      it('should sign up a user', (done) => {
-        const user = {
-          email: 'grants@yahoo.com',
-          firstname: 'Grant',
-          lastname: 'Nwaigwe',
-          username: 'glacos',
-          password: 'grants@1',
-          isAdmin: true,
-        };
-        request(app)
-          .post('/api/v1/users')
-          .send(user)
-          .expect(200, done());
-      });
-    });
-
-    describe('Handling Invalid case', () => {
-      it('should fail to sign up a user', (done) => {
-        const user = {
-          email: 'akugbeode@yahoo.com',
-          firstname: 'Akugbe',
-          lastname: 'Ode',
-          username: 'blaize3',
+describe('API endpoint Test for Event Manager', () => {
+  describe('Handle valid case', () => {
+    it('should create a user account', (done) => {
+      api(app).post('/api/v1/users')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'huekkjohn@trailblazer.com',
+          firstname: 'eeeeeeJohnson',
+          lastname: 'eeeJoel',
+          username: 'eeej2john',
           password: 'oghogho@1',
-          isAdmin: true,
-        };
-        request(app)
-          .post('/api/v1/users')
-          .send(user)
-          .expect(400, done());
-      });
+          isAdmin: false,
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((error, response) => {
+          expect(response.status).to.equal(200);
+          if (error) return done(error);
+          done();
+        });
+    });
+  });
+
+  describe('Handle Invalid case', () => {
+    it('should fail to create a user account', (done) => {
+      api(app).post('/api/v1/users')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'johntrail',
+          firstname: 'John',
+          lastname: 'Joel',
+          username: 'j2john',
+          password: 'oghogho@1',
+          isAdmin: 'false',
+        })
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .end((error, response) => {
+          console.log(response.body);
+          expect(response.status).to.equal(400);
+          if (error) return done(error);
+          done();
+        });
     });
   });
 });
 
-describe('', () => {
-  describe('Test Sigin In Route', () => {
-    describe('Handling valid case', () => {
-      it('should sign in a user', (done) => {
-        const user = {
-          email: 'akugbeode@yahooo.com',
+// sign in
+
+describe('API endpoint Test for Event Manager', () => {
+  describe('Handle valid case', () => {
+    it('should should into an account', (done) => {
+      api(app).post('/api/v1/users/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'akugbeode@yahoo.com',
           password: 'oghogho@1'
-        };
-        request(app)
-          .post('/api/v1/users/login')
-          .send(user)
-          .expect(200, done());
-      });
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((error, response) => {
+          expect(response.status).to.equal(200);
+          if (error) return done(error);
+          done();
+        });
     });
+  });
 
-
-    describe('Handling Invalid case', () => {
-      it('Sign in a User', (done) => {
-        const user = {
-          email: 'akugbeode@yahooo.com',
-          password: 'oghogh'
-        };
-        request(app)
-          .post('/api/v1/users/login')
-          .send(user)
-          .expect(401, done());
-      });
+  describe('Handle Invalid case', () => {
+    it('should fail to login into an account', (done) => {
+      api(app).post('/api/v1/users/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'akugbeode@yahoo.com',
+          password: 'ogho'
+        })
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .end((error, response) => {
+          console.log(response.body);
+          expect(response.status).to.equal(400);
+          if (error) return done(error);
+          done();
+        });
     });
   });
 });
-
-describe('', () => {
-  describe('Create an event', () => {
-    describe('Handling valid case', () => {
-      it('should create an event', (done) => {
-        const event = {
-          userId: 1,
-          centerId: 1,
-          title: 'Derek\'s Wedding',
-          organizer: 'esKimo Events',
-          start_date: '2018-01-04',
-          event_time: '12:00:00 am',
-          end_date: '2018-01-04',
-          privacy: 'public',
-          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta error in sapiente odit sit praesentium! Sapiente magnam tempore porro, quis provident aperiam quas nulla mollitia nam, laborum dolorum quia eaque.',
-        };
-        request(app)
-          .post('/api/v1/events')
-          .send(event)
-          .expect(200, done());
-      });
-    });
-  });
-
-
-  describe('Handling Invalid case', () => {
-    it('should fail to create an event', (done) => {
-      const event = {
-        userId: '',
-        centerId: 1,
-        title: '',
-        organizer: '',
-        start_date: '2018-01-04',
-        event_time: '12:00:00 am',
-        end_date: '2018-01-04',
-        privacy: 'public',
-        description: ''
-      };
-      request(app)
-        .post('/api/v1/events')
-        .send(event)
-        .expect(400, done());
-    });
-  });
-});
-
